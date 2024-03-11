88839e881fb8b76475e130904240fd3b78fabf07
mengva chuepor <you@example.com>
2024-03-11 01:26:26 -0500

    first commit

---
+++ ch_2cw2_10/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ch_2cw2_10/HomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const home(),
+    );
+  }
+}
---
+++ ch_2cw2_12/android/app/src/main/kotlin/com/example/ch_2cw2_12/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ch_2cw2_12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
